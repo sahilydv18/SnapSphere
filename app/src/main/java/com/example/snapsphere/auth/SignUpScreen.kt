01f1960f31ade52c1eebc8602cb09014e353cec8
Sahil Yadav <raosahil15266@gmail.com>
2024-09-06 11:34:48 +0530

    added signup screen UI

--- app/src/main/java/com/example/snapsphere/auth/SignUpScreen.kt
+++ app/src/main/java/com/example/snapsphere/auth/SignUpScreen.kt
@@ -1,12 +1,42 @@
 package com.example.snapsphere.auth
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.snapsphere.R
 import com.example.snapsphere.viewmodel.IgViewModel
 
 // sign up screen
@@ -17,10 +47,186 @@ fun SignUpScreen(
     igViewModel: IgViewModel
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Sign Up Screen")
+
+        var username by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        var email by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        var password by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        var showPassword by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        var confirmPassword by rememberSaveable {
+            mutableStateOf("")
+        }
+
+        var showConfirmPassword by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        var error by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        LaunchedEffect(password, confirmPassword) {
+            if(password.isNotBlank() && confirmPassword.isNotBlank()) {
+                error = password != confirmPassword
+            }
+        }
+
+        // app name and image
+        Row(
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.snapsphere_icon),
+                contentDescription = "app_icon",
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                modifier = Modifier.padding(16.dp),
+                fontSize = MaterialTheme.typography.displayMedium.fontSize,
+                fontFamily = FontFamily.Cursive,
+                style = TextStyle(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Color(0xFFFF6F61), Color(0xFF9B59B6), Color(0xFF008080))
+                    )
+                ),
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // welcome text
+        Text(
+            text = stringResource(id = R.string.welcome),
+            fontWeight = FontWeight.Bold,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // username text field
+        OutlinedTextField(
+            value = username,
+            onValueChange = {
+                username = it
+            },
+            label = {
+                Text(text = stringResource(id = R.string.username))
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.username_placeholder))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // email text field
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = stringResource(id = R.string.email))
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.email_placeholder))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // password text field
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = stringResource(id = R.string.password))
+            },
+            trailingIcon = {
+                IconButton(onClick = { showPassword = !showPassword }) {
+                    Icon(
+                        painter = painterResource(id = if(showPassword) R.drawable.baseline_visibility_24 else R.drawable.baseline_visibility_off_24),
+                        contentDescription = null
+                    )
+                }
+            },
+            visualTransformation = if(showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // password confirm text field
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = {
+                confirmPassword = it
+            },
+            label = {
+                Text(text = stringResource(id = R.string.confirm_password))
+            },
+            trailingIcon = {
+                IconButton(onClick = { showConfirmPassword = !showConfirmPassword }) {
+                    Icon(
+                        painter = painterResource(id = if(showConfirmPassword) R.drawable.baseline_visibility_24 else R.drawable.baseline_visibility_off_24),
+                        contentDescription = null
+                    )
+                }
+            },
+            visualTransformation = if(showConfirmPassword) VisualTransformation.None else PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            isError = password != confirmPassword && confirmPassword.isNotBlank(),
+            supportingText = {
+                if(error) {
+                    Text(text = stringResource(id = R.string.confirm_password_supporting_text))
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // sign up button
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = username.isNotBlank() && email.isNotBlank() && password.isNotBlank() && password == confirmPassword
+        ) {
+            Text(text = stringResource(id = R.string.sign_up))
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        // go to login screen text button
+        Row(
+            modifier = Modifier.padding(vertical = 16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.have_an_account),
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.login))
+            }
+        }
     }
 }
